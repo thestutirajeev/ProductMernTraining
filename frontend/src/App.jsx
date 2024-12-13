ab20419911ac6822b705b408435adf8cc8e2c182
Stuti Rajeev <stutirajeev5@gmail.com>
2024-12-13 23:18:16 +0530

    frontend 1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@chakra-ui/react"
+import { Routes, Route } from "react-router-dom"
+import CreatePage from "./pages/CreatePage.jsx"
+import HomePage from "./pages/HomePage.jsx"
+import Navbar from "./components/Navbar.jsx"
+function App() {
+  return (
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
+}
+
+export default App;
