4282722f6050c5b1ffbddc86b17a5c65db815f9c
Stuti Rajeev <stutirajeev5@gmail.com>
2024-12-13 23:17:53 +0530

    backend

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -40,11 +40,14 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProduct = async(req, res) => {
     const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: 'Invalid product id'});
+    }
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success: true, message: 'Product is deleted'});
     } catch (error) {
         console.error('Error: in delete product', error.message);
-        res.status(404).json({success: false, message: 'Product not found'});
+        res.status(500).json({success: false, message: 'Server Error'});
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ import productRoutes from './routes/product.routes.js';
 dotenv.config();
 
 const app = express(); 
-
+const PORT = process.env.PORT || 5000;
 app.use(express.json());//allows us to accept json data in the req.body
 
 //POSTMAN TEST instead of frontend
 console.log(process.env.MONGO_URI);//check if the connection is successful
 app.use('/api/products',productRoutes);
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server is running on port 5000 at http://localhost:5000');
+    console.log('Server is running on port 5000 at http://localhost:' + PORT);
  });
