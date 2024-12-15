82ad5eb590d6d1814e611f5734a5bca3cb724c59
Stuti Rajeev <stutirajeev5@gmail.com>
2024-12-15 23:04:22 +0530

    server.js altered to connect with frontend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
-import productRoutes from './routes/product.routes.js';  
+import cors from 'cors';
+import productRoutes from './routes/product.routes.js';
 
 dotenv.config();
-
 const app = express(); 
 const PORT = process.env.PORT || 5000;
+
+// Use CORS middleware
+app.use(cors());
 app.use(express.json());//allows us to accept json data in the req.body
 
 //POSTMAN TEST instead of frontend
-console.log(process.env.MONGO_URI);//check if the connection is successful
+console.log(process.env.MONGO_URI);//check if the connection is successful''
+
 app.use('/api/products',productRoutes);
+
 app.listen(PORT, () => {
     connectDB();
     console.log('Server is running on port 5000 at http://localhost:' + PORT);
